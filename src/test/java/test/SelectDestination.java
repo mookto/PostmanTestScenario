4bbfa4c1be01cf350383790e102ebb591d8c603e
IKBAl <45hasanmahedi@gmail.com>
2020-12-12 11:48:38 +0600

    This is my first my commit

---
+++ src/test/java/test/SelectDestination.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelectDestination {
+
+	
+
+		
+	
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\IQBAL HOSSAIN\\eclipse-workspace\\TestProject\\src\\Drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.spicejet.com/");
+		driver.findElement(By.xpath("//span[@id='ctl00_mainContent_ddl_originStation1_CTXTaction']")).click();
+		List <WebElement> city1=driver.findElements(By.className("dropdownDiv"));
+		for (WebElement webcity1 : city1) {
+			String cityname=webcity1.getText();
+			System.out.println(cityname);
+			
+			
+			 
+		//driver.findElement(By.xpath("//a[contains(text(),'Jammu (IXJ)')]")).click();
+		
+		
+
+	}
+		
+	}}
---
+++ src/test/java/test/Test2.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+	public static void main(String[] args) {Scanner entercity =new Scanner(System.in);
+	String city;
+	city=entercity.toString();
+
+	}
+
+}
